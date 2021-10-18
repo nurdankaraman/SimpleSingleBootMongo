49ae1ea7c18e69eeaf680d6e4aef85c61d9aee25
nurdan <nurdan.karamann@gmail.com>
2021-10-18 16:07:05 +0300

    sending the first time related to demo spring boot react project

---
+++ src/main/java/com/example/controller/StudentController.java
@@ -0,0 +1,84 @@
+package com.example.controller;
+
+import com.example.entity.Student;
+import com.example.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/student")
+public class StudentController {
+
+    @Autowired
+    StudentService studentService;
+
+    @PostMapping("/create")
+    public Student createStudent(@RequestBody Student student) {
+        return studentService.createStudent(student);
+    }
+
+    @GetMapping("getById/{id}")
+    public Student getStudentById(@PathVariable String id) {
+        return studentService.getStudentById(id);
+
+    }
+
+    @GetMapping("/all")
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @PutMapping("/update")
+    public Student updateStudent(@RequestBody Student student) {
+        return studentService.updateStudent(student);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteStudent(@PathVariable String id) {
+        return studentService.deleteStudent(id);
+    }
+
+    @GetMapping("/studentsByName/{name}")
+    public List<Student> studentByName(@PathVariable String name) {
+        return studentService.getStudentsByName(name);
+    }
+
+    @GetMapping("/studentsByNameAndMail")
+    public List<Student> studentsByNameAndMail(@RequestParam String name, @RequestParam String email) {
+        return studentService.studentsByNameAndMail(name, email);
+    }
+
+    @GetMapping("/studentsByNameOrMail")
+    public List<Student> studentsByNameOrMail(@RequestParam String name, @RequestParam String email) {
+        return studentService.studentsByNameOrMail(name, email);
+    }
+
+    @GetMapping("/allWithPagination")
+    public List<Student> getAllWithPagination(@RequestParam int pageNo, @RequestParam int pageSize) {
+        return studentService.getAllWithPagination(pageNo, pageSize);
+    }
+
+    @GetMapping("/allWithSorting")
+    public List<Student> allWithSorting() {
+        return studentService.allWithSorting();
+    }
+
+    @GetMapping("/byDepartmentName")
+    public List<Student> byDepartmentName(@RequestParam String deptName){
+        return studentService.byDepartmentName(deptName);
+    }
+    @GetMapping("/bySubjectName")
+    public List<Student> bySubjectName(@RequestParam String subName){
+        return studentService.bySubjectName(subName);
+    }
+    @GetMapping("/emailLike")
+    public List<Student> emailLike(@RequestParam String email){
+        return studentService.emailLike(email);
+    }
+    @GetMapping("/nameStartsWith")
+    public List<Student> nameStartsWith(@RequestParam String name){
+        return studentService.nameStartsWith(name);
+    }
+}
---
+++ src/main/java/com/example/repository/StudentRepository.java
@@ -0,0 +1,24 @@
+package com.example.repository;
+
+import com.example.entity.Student;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StudentRepository extends MongoRepository<Student, String> {
+
+    List<Student> findByName(String name);
+
+    List<Student> findByNameAndEmail(String name, String email);
+
+    List<Student> findByNameOrEmail(String name, String email);
+
+    List<Student> findByDepartmentDepartmentName(String deptName);
+
+    List<Student> findBySubjectsSubjectName(String subName);
+
+    List<Student> findByEmailIsLike(String email);
+    List<Student> findByNameStartsWith(String name);
+}
